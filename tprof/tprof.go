1c76b0302a663c4a100d4d1a16ea622d76341cd8
mikewang <it512@msn.com>
2019-01-01 02:16:38 +0800

    Mouter

--- tprof/tprof.go
+++ tprof/tprof.go
@@ -11,3 +11,19 @@ var TprofCmdLine = twig.WrapHttpHandlerFunc(pprof.Cmdline)
 var TprofProfile = twig.WrapHttpHandlerFunc(pprof.Profile)
 var TprofSymbol = twig.WrapHttpHandlerFunc(pprof.Symbol)
 var TprofTrace = twig.WrapHttpHandlerFunc(pprof.Trace)
+
+type Prefix string
+
+func (t Prefix) String() string {
+	return string(t)
+}
+
+func (t Prefix) Mount(mux twig.Register) {
+	mux.AddHandler(twig.GET, t.String()+"/", TprofIndex)
+	mux.AddHandler(twig.GET, t.String()+"/*", TprofIndex)
+
+	mux.AddHandler(twig.GET, t.String()+"/cmdline", TprofCmdLine)
+	mux.AddHandler(twig.GET, t.String()+"/profile", TprofProfile)
+	mux.AddHandler(twig.GET, t.String()+"/symbol", TprofSymbol)
+	mux.AddHandler(twig.GET, t.String()+"/trace", TprofTrace)
+}
